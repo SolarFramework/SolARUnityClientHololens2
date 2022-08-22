b13c94e6d75bdf1337c5dfeea7c81b35fb0517c6
Jean-Marie HENAFF <Jean-Marie.HENAFF@b-com.com>
2022-08-22 15:58:18 +0200

    Remove gRPC directory for it contains obsolete content

--- gRPC/envoy/build-wsl-to-win-envoy-config.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cat /etc/resolv.conf | grep nameserver | cut -d " " -f 2 | xargs -I foo sed 's/                address: 0.0.0.0/                address: foo/g' envoy-config.yaml &> envoy-config-to-win.yaml
